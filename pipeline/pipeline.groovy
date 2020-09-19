21efcba7c3815ca5c36ee9269c1e5475e0a4e2c3
Manuel Alejandro Echeverry <manuel.echeverry.7671@eam.edu.co>
2020-09-19 02:51:03 -0500

    last commit

--- pipeline/pipeline.groovy
+++ pipeline/pipeline.groovy
@@ -49,8 +49,8 @@ pipeline {
         
         stage('Deploy') {
             steps{
-                sh "sed -i 's:DOCKER_IMAGE:${env.DOCKER_IMAGE}:g' ${DEPLOY_FOLDER}/deployment-back.yaml"
-                sh "sed -i 's:TAG:${tag}:g' ${DEPLOY_FOLDER}/deployment-back.yaml"
+                sh "sed -i 's:DOCKER_IMAGE:${env.DOCKER_IMAGE}:g' ${DEPLOY_FOLDER}/deployment.yaml"
+                sh "sed -i 's:TAG:${tag}:g' ${DEPLOY_FOLDER}/deployment.yaml"
                 
                 step([$class: 'KubernetesEngineBuilder', 
                         projectId: "nice-root-288300",
